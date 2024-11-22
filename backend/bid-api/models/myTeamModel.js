acd434b0d9583fef67a2f7b4eae3852dd79d5187
ravi <ravima450@gmail.com>
2024-11-22 22:32:50 +0530

    current user

--- backend/bid-api/models/myTeamModel.js
+++ backend/bid-api/models/myTeamModel.js
@@ -12,6 +12,20 @@ exports.getRecordByTeam = async (team)=>{
     const spendAmount = await pool
     .request()
     .input('team', team)
-    .query('SELECT SUM(prize) AS total FROM completed_list WHERE tname= @team');
-    return {ok:team, members : teamMembers?.recordset[0], spendAmount:spendAmount?.recordset[0]}
+    .query(`
+        SELECT * FROM (
+SELECT 
+    completed_list.tname, 
+    SUM(completed_list.prize) AS spent_prize, 
+    COUNT(completed_list.pname) AS players, 	
+    teamList.availableprize
+FROM 
+    completed_list
+INNER JOIN 
+    teamList ON completed_list.tname = teamList.teamName
+GROUP BY 
+    teamList.availableprize,
+	completed_list.tname) AS result where tname = @team`);
+    console.log(teamMembers)
+    return {ok:team, teamData: spendAmount.recordset[0], playerData : teamMembers.recordset[0]}
 }
